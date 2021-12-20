a5d16533723b73fd955d326a2ada58226a6b10d8
Lucas Zluhan <lszluhan@hotmail.com>
2021-12-20 20:12:24 -0300

    att

--- ormconfig.js
+++ ormconfig.js
@@ -5,9 +5,9 @@ module.exports = {
    url: process.env.DATABASE_URL,
    synchronize: false,
    logging: false,
-   entities: ['.dist/src/core/data/database/entities/**/*', 'src/core/data/database/entities/**/*'],
+   entities: ['src/core/data/database/entities/*.ts', './.dist/core/data/database/entities/*.js'],
    cli: {
-      entitiesDir: 'src/core/data/database/entities/**/*',
+      entitiesDir: 'src/core/data/database/entities',
    },
    extra: {
       ssl: {
